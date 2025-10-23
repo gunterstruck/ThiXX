f7ab43e1566b5b3d4332fc8a7b383b5cd8780992
gunterstruck <224373224+gunterstruck@users.noreply.github.com>
2025-10-23 20:08:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_CACHE_NAME = 'thixx-robust-v6-28'; // Version erhöht, um Update auszulösen
+const APP_CACHE_NAME = 'thixx-robust-v6-29'; // Version erhöht, um Update auszulösen
 const DOC_CACHE_NAME = 'thixx-docs-v1';
 
 /*
